ea2bce7118a478ff903dac81195e99fce6b26ba5
u16orange <u16orange@gmail.com>
2022-07-04 15:56:49 +0000

    resubmit

--- team.rb
+++ team.rb
@@ -5,49 +5,35 @@ class Team
   attr_accessor :name, :win, :lose, :draw
 
   # インスタンスを初期化するための、特別なメソッド
-  def initialize(team_name)
-    self.name = team_name
-    if name == "Giants"
-      self.win = 67
-      self.lose = 45
-      self.draw = 8
-    elsif name == "Tigers"
-      self.win = 60
-      self.lose = 53
-      self.draw = 7
-    elsif name == "Dragons"
-      self.win = 60
-      self.lose = 55
-      self.draw = 5
-    elsif name == "BayStars"
-      self.win = 56
-      self.lose = 58
-      self.draw = 6
-    elsif name == "Carp"
-      self.win = 52
-      self.lose = 56
-      self.draw = 12
-    elsif name == "Swallows"
-      self.win = 41
-      self.lose = 69
-      self.draw = 10
-    end
+  def initialize(name, win, lose, draw)
+    self.name = name
+    self.win = win
+    self.lose = lose
+    self.draw = draw
   end
   
-    # インスタンスが持つメソッド（処理）
   def calc_win_rate
     self.win.to_f / (self.win.to_f + self.lose.to_f)
   end
   
   def show_team_result
-    puts "#{self.name}の2020年の成績は#{self.win}勝#{self.lose}敗#{self.draw}分、勝率は#{self.calc_win_rate}す。"
+    puts "#{self.name} の2020年の成績は #{self.win}勝 #{self.lose}敗 #{self.draw}分、勝率は #{self.calc_win_rate}です。"
   end
+  
 end
 
-["Giants", "Tigers", "Dragons", "BayStars", "Carp", "Swallows"].each do |team|
-  # インスタンスの生成と、変数への代入
-  team_grades = Team.new(team)
-  
-  # インスタンスの使用
-  team_grades.show_team_result
-end
+# インスタンスの生成と、変数への代入
+team1 = Team.new("Giants", 67, 45, 8)
+team2 = Team.new("Tigers", 60, 53, 7)
+team3 = Team.new("Dragons", 60, 55, 5)
+team4 = Team.new("BayStars", 56, 58, 6)
+team5 = Team.new("Carp", 52, 56, 12)
+team6 = Team.new("Swallows", 41, 69, 10)
+
+# インスタンスの使用
+team1.show_team_result
+team2.show_team_result
+team3.show_team_result
+team4.show_team_result
+team5.show_team_result
+team6.show_team_result
